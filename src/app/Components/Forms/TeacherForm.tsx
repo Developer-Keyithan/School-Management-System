aecb781d299363572980f42f14631cd7de467b18
Developer-Keyithan <sathyjaseelankeyithan@gmail.com>
2025-03-04 21:12:23 +0530

    Create teacher form on process

--- src/app/Components/Forms/TeacherForm.tsx
+++ src/app/Components/Forms/TeacherForm.tsx
@@ -1,5 +1,21 @@
 'use client'
 
+import { z } from "zod";
+
+const schema = z.object({
+    userName: z
+        .string()
+        .min(3, { message: 'User name must be at least 3 characters long!' })
+        .max(20, { message: 'User name must be at least 3 characters long!' }),
+    email: z.string().email({message: 'Invalid email address!'}),
+    password: z.string().min(8, { message: 'Password must be at least 8 characters long!' }),
+    firstName: z.string().min(8, { message: 'First name is required!' }),
+    lastName: z.string().min(8, { message: 'Last name is required!' }),
+    phone: z.string().min(8, { message: 'Phone number is required!' }),
+    address: z.string().min(8, { message: 'Address is required!' }),
+    birthDay: z.date({ message: 'Birthday is required!' })
+});
+
 const TeacherForm = ({
     type,
     data
